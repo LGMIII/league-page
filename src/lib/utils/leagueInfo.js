73b774543e5376f6554260447caaa26c8cbbb33d
LGMIII <106564960+LGMIII@users.noreply.github.com>
2024-05-01 21:21:18 -0400

    My Leagueinfo website

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "VkxL84ZBQY80"; // your league ID
+export const leagueID = "1091240248136265728"; // your league ID
 export const leagueName = "Trouble Shooting"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
